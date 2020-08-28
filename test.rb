3e9eb64d975cd0575d0917983cc824cbdcc4deb6
jajapop <miki_yudai_19891111@yahoo.co.jp>
2020-08-29 01:47:02 +0900

    add_paiza

--- test.rb
+++ test.rb
@@ -1,9 +1,15 @@
-loop do
-  m,f,r = gets.split.map(&:to_i)
-  break if m == -1 && f == -1 && r == -1
-  next puts "F" if m == -1 || f == -1 || m+f < 30
-  next puts "D" if m+f < 50 && r < 50
-  next puts "C" if m+f < 65
-  next puts "B" if m+f < 80
-  next puts "A"
+n = gets.to_i
+d = gets.split.map(&:to_i)
+ans = 6
+max = []
+d.each_cons(7) do |di|
+  if di.count(0) >= 2
+    ans += 1
+  else
+    max << ans
+    ans = 6
+  end
 end
+max << ans
+ans = max.max
+puts ans == 6 ? 0 : ans
